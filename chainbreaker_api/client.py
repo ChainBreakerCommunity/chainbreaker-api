b4b3296640ea188741c86817748b89549a1152ec
Juanchobanano <juancepeda.gestion@gmail.com>
2021-08-31 22:32:36 -0500

    Versión 0.0.7 de ChainBreaker

--- chainbreaker_api/client.py
+++ chainbreaker_api/client.py
@@ -10,7 +10,7 @@ import pandas as pd
 class ChainBreakerClient():
 
     def __init__(self, endpoint):
-        self.__endpoint = endpoint #"https://chainbreaker-ibm-grumpy-gecko-lm.mybluemix.net/"
+        self.__endpoint = endpoint
         self.__name = None
         self.__email = None
         self.__permission = None
@@ -57,13 +57,8 @@ class ChainBreakerClient():
             self.__name = res["name"]
             self.__email = res["email"]
             self.__permission = res["permission"]
-            #img = Image.open("assets/images/chainbreaker.png")
-            #plt.imshow(img)
-            #plt.axis("off")
-            #plt.show()
             return "Hi {}! You are now connected to ChainBreaker API. Your current permission level is '{}'. If you have any questions don't hesitate to contact us!".format(self.__name, self.__permission)
-        else: 
-            print(res.text)
+        print(res.text)
     
     def logout(self):
         """
@@ -143,21 +138,28 @@ class ChainBreakerClient():
         """
         return self.__token
     
-    def get_ads(self, language = "", website = ""): #, features = True, locations = False, comments = False, emails = False, names = False, phone = False, whatsapp = False):
+    def get_ads(self, language = "", website = "", filter_by_phones = False, filter_by_location = False): #, features = True, locations = False, comments = False, emails = False, names = False, phone = False, whatsapp = False):
         """
         This function returns ads data from ChainBreaker Database.
         - language can be: "spanish", "english" or "" (all).
         - website can be: 
           - "mileroticos", "skokka" or "" (all) (for "spanish")
           - "leolist" or "" (all) (for "english")
+        - filter_by_phones: Boolean. Default value: False
+          - If True, you will recieve only the ads that contain a phonenumber.
+        - filter_by_location: Boolean. Default value: False
+          - If True, you will recieve only the ads that contain a location
         """
         if self.__token != None:
             #data = {"features": str(features), "locations": str(locations), "comments": str(comments), "emails": str(emails), "names": str(names), "phone": str(phone), "whatsapp": str(whatsapp)}
-            data = {"language": language, "website" : website}
+            filter_by_phones = 0 if filter_by_phones == False else 1
+            filter_by_location = 0 if filter_by_location == False else 1
+            
+            data = {"language": language, "website" : website, "filter_by_phones": filter_by_phones, "filter_by_location": filter_by_location}
             headers = {"x-access-token": self.__token}
             res = requests.post(self.__endpoint + "/api/data/get_ads", data = data, headers = headers).json()["ads"]
             df = pd.DataFrame(res)
-            columns = ["id_ad", "language", "link", "id_page", "title", "text", "category", "post_date", "verified_ad", "no_prepayment", "promoted_ad", "external_website", "reviews_website", "website", "extract_date"]
+            columns = ["id_ad", "data_version", "author", "language", "link", "id_page", "title", "text", "category", "post_date", "extract_date", "website", "whatsapp", "verified_ad", "prepayment", "promoted_ad", "external_website", "reviews_website"]
             df = df[columns]
             df.set_index("id_ad", inplace = True)
             return df
@@ -168,6 +170,8 @@ class ChainBreakerClient():
         """
         This function returns the glossary of terms contained in ChainBreaker Database.
         - domain can be: "sexual", "general" or "" (all).
+        This glossary were shared by Lena Garrett from Stop The Traffik.
+        For more information please contact her: Lena.Garrett@stopthetraffik.org
         """
         if self.__token != None:
             data = {"domain": domain}
@@ -185,6 +189,8 @@ class ChainBreakerClient():
         """
         This function returns the set of keywords contained in ChainBreaker Database
         - language can be: "english", "spanish", "portuguese", "russian" or "" (all).
+        These keywords were shared by Lena Garrett from Stop The Traffik.
+        For more information please contact her: Lena.Garrett@stopthetraffik.org
         """
         if self.__token != None:
             data = {"language": language}
